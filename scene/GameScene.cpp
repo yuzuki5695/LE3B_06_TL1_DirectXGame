f9a56f5b764f2254bec215fd0da01313ae270fe5
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-06-06 10:47:58 +0900

    プロジェクト ファイルを追加します。

---
+++ scene/GameScene.cpp
@@ -0,0 +1,194 @@
+﻿#include "GameScene.h"
+#include "TextureManager.h"
+#include <cassert>
+
+#include<json.hpp>
+#include <fstream>
+
+GameScene::GameScene() {}
+
+GameScene::~GameScene() {}
+
+void GameScene::Initialize() {
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+	debugText_ = DebugText::GetInstance();
+
+	// 連結してフルパスを得る
+	const std::string fullpath = std::string("Resources/levels/") + "untitled.json";
+
+	// ファイルストリーム
+	std::ifstream file;
+
+	// ファイルを開く
+	file.open(fullpath);
+	// ファイルオープン失敗をチェック
+	if (file.fail()) {
+		assert(0);
+	}
+
+	// JSON文字列から解凍したデータ
+	nlohmann::json deserialized;
+	
+	// 解凍
+	file >> deserialized;
+
+	// 正しいレベルデータファイルかチェック
+	assert(deserialized.is_object());
+	assert(deserialized.contains("name"));
+	assert(deserialized["name"].is_string());
+
+	// "name"を文字列として
+	std::string name = deserialized["name"].get<std::string>();
+	// 正しいレベルデータファイルかチェック
+	assert(name.compare("scene") == 0);
+
+	// レベルデータ格納用インスタンスを生成
+	levelData = new LevelData();
+
+	// "worldTransform"の全オブジェクトを走査
+	for (nlohmann::json& object : deserialized["objects"]) {
+		assert(object.contains("type"));
+
+		// 種別を取得
+		std::string type = object["type"].get<std::string>();
+
+		// 種類ごとの処理
+
+		// MESH
+		if (type.compare("MESH") == 0) {
+			// 要素追加
+			levelData->objects.emplace_back(LevelData::ObjectData{});
+			// 今追加した要素の参照を得る
+			LevelData::ObjectData& objectData = levelData->objects.back();
+
+			if (object.contains("file_name")) {
+				// ファイル名
+				objectData.fileName = object["file_name"];
+			}
+
+
+			// トランスフォームのパラメータ読み込み
+			nlohmann::json& transform = object["transform"];
+			// 平行移動
+			objectData.translation.x = (float)transform["translation"][0];
+			objectData.translation.y = (float)transform["translation"][2];
+			objectData.translation.z = (float)transform["translation"][1];
+			// 回転角
+			objectData.rotation.x = -(float)transform["rotation"][0];
+			objectData.rotation.y = -(float)transform["rotation"][2];
+			objectData.rotation.z = -(float)transform["rotation"][1];
+			// スケーリング
+			objectData.scaling.x = (float)transform["scaling"][0];
+			objectData.scaling.y = (float)transform["scaling"][2];
+			objectData.scaling.z = (float)transform["scaling"][1];
+
+			// TODO: コライダーのパラメータ読み込み
+
+			// TODO: オブジェクト走査を再起関数にまとめ、再起呼出で技を走査する
+			if (object.contains("children")) {
+
+			}
+		}
+	}
+
+	// レベルデータに出現するモデルの読み込み
+	for (auto& objectData : levelData->objects) {
+		// ファイル名から登録済みモデルを検索
+		decltype(models)::iterator it = models.find(objectData.fileName);
+
+		// 見込みの場合、読み込む
+		if (it == models.end()) {
+			Model* model = Model::CreateFromOBJ(objectData.fileName);
+			models[objectData.fileName] = model;
+		}
+	}
+
+	// レベルデータからワールド行列を生成
+	for (auto& objectData : levelData->objects) {
+		// ワールド行列を生成
+		WorldTransform* worldTransform = new WorldTransform();
+		// 座標
+		worldTransform->translation_ = objectData.translation;
+		// 回転角
+		worldTransform->rotation_ = objectData.rotation;
+		// スケーリング
+		worldTransform->scale_ = objectData.scaling;
+
+		worldTransform->Initialize();
+
+		// 配列に登録
+		worldTransforms.push_back(worldTransform);
+	}
+	viewProjection.Initialize();
+}
+
+void GameScene::Update() {
+	for (WorldTransform* worldTransform : worldTransforms) {
+		// TDOO:: ワールド行列の計算
+		//worldTransforms
+		worldTransform->TransferMatrix();
+	}
+}
+
+void GameScene::Draw() {
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+			
+	int i = 0;
+	// レベルデータからオブジェクトを生成、配置
+	for (auto& objectData : levelData->objects) {
+		Model* model = nullptr;
+		// ファイル名から登録済みモデルを検索
+		decltype(models)::iterator it = models.find(objectData.fileName);
+		if (it != models.end()) { model = it->second; }
+		model->Draw(*worldTransforms[i], viewProjection);
+
+		i++;
+	}
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// デバッグテキストの描画
+	debugText_->DrawAll(commandList);
+	//
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
---
+++ scene/GameScene.h
@@ -0,0 +1,80 @@
+﻿#pragma once
+
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "DebugText.h"
+#include "Input.h"
+#include "Model.h"
+#include "SafeDelete.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
+#include<map>
+#include<string>
+
+/// <summary>
+/// ゲームシーン
+/// </summary>
+class GameScene {
+public: // メンバ関数	
+	// レベルデータ
+	struct LevelData {
+		// オブジェクト1個分のデータ
+		struct ObjectData {
+			// ファイル名
+			std::string fileName;
+			Vector3 translation;
+			Vector3 rotation;
+			Vector3 scaling;
+		};
+		// オブジェクトのコンテナ
+		std::vector<ObjectData> objects;
+	};
+public: // メンバ関数
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	GameScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+private: // メンバ変数
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+	DebugText* debugText_ = nullptr;
+
+	/// <summary>
+	/// ゲームシーン用
+	/// </summary>
+
+
+	// レベルデータ格納用インスタンスを生成
+	LevelData* levelData = nullptr;
+	// モデルデータコンテナ
+	std::map<std::string, Model*> models;
+
+	// ワールド行列
+	std::vector<WorldTransform*> worldTransforms;
+
+	ViewProjection viewProjection;
+};
